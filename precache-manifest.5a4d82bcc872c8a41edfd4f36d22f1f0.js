c5825422dfebf5e9387f6e23bf9fc445374bc670
Thiago Paiva <tdesousapaiva@gmail.com>
2025-05-09 01:36:05 -0300

    Updates

---
+++ precache-manifest.5a4d82bcc872c8a41edfd4f36d22f1f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25ab649843763791f28a85d0631c0828",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9396ed610622a9f467a2",
+    "url": "/portfolio/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "ec0935851c1edf4cb708",
+    "url": "/portfolio/static/js/2.9e11b540.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/portfolio/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "9396ed610622a9f467a2",
+    "url": "/portfolio/static/js/main.9d621f38.chunk.js"
+  },
+  {
+    "revision": "7be6fcfa3cfae298e923",
+    "url": "/portfolio/static/js/runtime~main.4caf4deb.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/portfolio/static/media/logo.06e73328.svg"
+  }
+]);
